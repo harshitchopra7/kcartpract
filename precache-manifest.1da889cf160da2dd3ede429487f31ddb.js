2b60fd751f71726a89bf96a7f1d6a0fab002de7b
Harshit Chopra <chopraharshit2000@gmail.com>
2020-09-09 02:19:08 +0530

    Updates

---
+++ precache-manifest.1da889cf160da2dd3ede429487f31ddb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245faf1713ecbcf240302854a11504d0",
+    "url": "/kcartpract/index.html"
+  },
+  {
+    "revision": "1a30edfadc7ab715f8ec",
+    "url": "/kcartpract/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2fc0589fca86792e0766",
+    "url": "/kcartpract/static/css/main.efc66221.chunk.css"
+  },
+  {
+    "revision": "1a30edfadc7ab715f8ec",
+    "url": "/kcartpract/static/js/2.2a66dffb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kcartpract/static/js/2.2a66dffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fc0589fca86792e0766",
+    "url": "/kcartpract/static/js/main.c1a5a4f9.chunk.js"
+  },
+  {
+    "revision": "70a2084de5beb1755293",
+    "url": "/kcartpract/static/js/runtime-main.ffd6d2ff.js"
+  }
+]);
